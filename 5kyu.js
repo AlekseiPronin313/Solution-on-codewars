a256e77b6ed537e3ae0599b3121253a789cc26c8
Pronin <isw.pronin@yandex.ru>
2022-12-27 19:09:16 +0500

    Push

---
+++ 5kyu.js
@@ -0,0 +1,4 @@
+// let incrementString = str => str.replace(/([0-8]|\d?9+)?$/, (e) => e ? + e + 1 : 1)
+// console.log(incrementString("fo99obar99"))
+// console.log(incrementString("foo"))
+// console.log(incrementString('1'))
--- 6kyu.js
+++ 6kyu.js
@@ -140,3 +140,31 @@
 // const solution = (string) => string.replace(/([A-Z])/g, " $1")
 // console.log(solution('camelCasing'))
 // console.log(solution('camelCasingTest'))
+
+
+// function likes(names) {
+//     if (names.length === 0) {
+//         return 'no one likes this'
+//     } else if (names.length === 1) {
+//         return `${names} likes this`
+//     } else if (names.length === 2) {
+//         return `${names[0]} and ${names[1]} like this`
+//     } else if (names.length === 3) {
+//         return `${names[0]}, ${names[1]} and ${names[2]} like this`
+//     } else {
+//         return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+//     }
+// }
+// console.log(likes(['Peter']))
+// console.log(likes([]))
+// console.log(likes(['Max', 'John', 'Mark']))
+// console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']))
+
+
+// function sortArray(array) {
+//     const sort = array.filter((x) => x % 2).sort((a,b) => a - b);
+//     return array.map((x) => x % 2 ? sort.shift() : x);
+// }
+// console.log(sortArray([]))
+// console.log(sortArray([5, 3, 2, 8, 1, 4]))
+// console.log(sortArray([5, 3, 1, 8, 0]))
