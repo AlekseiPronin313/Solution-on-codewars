6bf8c3f979623906fd444f2756b927bb6e679b44
Aleksei Pronin <91370169+AlekseiPronin313@users.noreply.github.com>
2023-04-11 14:36:33 +0500

    Push

--- JS/5kyu.js
+++ JS/5kyu.js
@@ -6,4 +6,3 @@
 // const domainName = (url) => url.match(/(?:https?:\/\/)?(?:www\.)?([^\/]+)/)[1].split('.')[0];
 // console.log(domainName("http://google.com"));
 // console.log(domainName("http://google.co.jp"));
-// console.log(domainName("www.xakep.ru"));
