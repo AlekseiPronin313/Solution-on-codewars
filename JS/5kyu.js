a1a3dcc3302b50d4cd4c31a8699413f2c1836c33
Aleksei Pronin <91370169+AlekseiPronin313@users.noreply.github.com>
2023-11-03 21:39:13 +0400

    Push

--- JS/5kyu.js
+++ JS/5kyu.js
@@ -3,6 +3,21 @@
 // console.log(incrementString("foo"))
 // console.log(incrementString('1'))
 
+
 // const domainName = (url) => url.match(/(?:https?:\/\/)?(?:www\.)?([^\/]+)/)[1].split('.')[0];
 // console.log(domainName("http://google.com"));
 // console.log(domainName("http://google.co.jp"));
+
+
+// function incrementString (str) {
+//     return str.replace(/(\d*)$/, function (match) {
+//         if (match === '') return '1'
+//         let number = String(parseInt(match, 10) +1)
+//         return match.length > number.length ? '0'.repeat(match.length - number.length) + number : number
+//     })
+// }
+// console.log(incrementString("foobar000"))
+// console.log(incrementString("foo"))
+// console.log(incrementString("1"))
+// console.log(incrementString("009"))
+// console.log(incrementString("fo99obar99"))
--- JS/6kyu.js
+++ JS/6kyu.js
@@ -262,3 +262,17 @@
 // }
 // console.log(findUniq([ 1, 0, 0 ]))
 // console.log(findUniq([ 3, 10, 3, 3, 3 ]))
+
+
+// function likes(names) {
+//     if (names.length === 0) return 'no one likes this'
+//     if (names.length === 1) return `${names} likes this`
+//     if (names.length === 2) return `${names[0]} and ${names[1]} like this`
+//     if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`
+//     if (names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+// }
+// console.log(likes([]))
+// console.log(likes(['Peter']))
+// console.log(likes(['Jacob', 'Alex']))
+// console.log(likes(['Max', 'John', 'Mark']))
+// console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']))
